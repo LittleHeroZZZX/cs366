d78dc879e554f47094824deadcdc2801551d0158
zhouxin <zhou.xin@mail.ustc.edu.cn>
2025-12-08 08:32:03 +0000

    Fix: decode and cpp build

--- assignment1-basics/src/nn/decode.py
+++ assignment1-basics/src/nn/decode.py
@@ -8,6 +8,7 @@ from .config import Config
 from .functional import softmax
 from .networks import TransformerLM
 from .utils import load_checkpoint
+import timeit
 
 
 def decode(
@@ -31,6 +32,7 @@ def decode(
     Returns:
         str: The generated text sequence.
     """
+    now = timeit.default_timer()
     save_path = Path(model_path)
     assert save_path.exists(), f"Model path {model_path} does not exist."
     config_path = save_path / "config.yaml"
@@ -39,6 +41,7 @@ def decode(
         checkpoint_files = list(save_path.glob("checkpoint_*.pt"))
         assert checkpoint_files, f"No checkpoint files found in {model_path}."
         checkpoint_number = max(int(f.stem.split("_")[1]) for f in checkpoint_files if f.stem.split("_")[1].isdigit())
+        logger.info(f"Using checkpoint number: {checkpoint_number}")
     checkpoint_path = save_path / f"checkpoint_{checkpoint_number}.pt"
     assert checkpoint_path.exists(), f"Checkpoint file {checkpoint_path} does not exist."
 
@@ -59,14 +62,16 @@ def decode(
 
     input_ids = tokenizer.encode(prompt)
     input_tensor = torch.tensor([input_ids], device=device)
+    token_count = 0
     while len(input_ids) < max_length:
+        token_count += 1
         logits: torch.Tensor = model(input_tensor)
-        logits = logits.squeeze()
-        assert logits.dim() == 1
+        logits = logits[:, -1, :]
+        logits = logits.flatten()
 
         logits = logits / temperature
         logits = softmax(logits, dim=-1)
-        logits = logits.sort(dim=-1, descending=True)[0]
+        logits, index = logits.sort(dim=-1, descending=True)
         count = 0
         cum_prob = 0.0
         while cum_prob < p and count < logits.size(0):
@@ -74,7 +79,7 @@ def decode(
             count += 1
         filtered_logits = logits[:count]
         filtered_logits = filtered_logits / filtered_logits.sum()
-        next_token_id = int(torch.multinomial(filtered_logits, num_samples=1).item())
+        next_token_id = index[int(torch.multinomial(filtered_logits, num_samples=1).item())].item()
         generated_ids = input_ids + [next_token_id]
         input_tensor = torch.tensor([generated_ids], device=device)
         input_ids = generated_ids
@@ -84,4 +89,5 @@ def decode(
             break
 
     generated_text = tokenizer.decode(input_ids)
+    logger.info(f"Generate speed: {token_count / (timeit.default_timer() - now):.2f} tok/s")
     return generated_text
